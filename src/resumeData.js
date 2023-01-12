dde736d9f80f45c6b1f40098ea7e4f18aaa192ef
Saad BENALI <109537271+saadasl@users.noreply.github.com>
2023-01-12 18:01:51 +0100

    Add files via upload

---
+++ src/resumeData.js
@@ -0,0 +1,88 @@
+let resumeData = {
+    "imagebaseurl":"https://github.com/saadasl",
+    "name": "saad benali",
+    "role": "full stack developer",
+    "linkedinId":"saad benali",
+    "skypeid": "saad benali",
+    "roleDescription": "I like dabbling in various parts of frontend and backend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/saad-benali-21878a193/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/saadasl",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"skype",
+          "url":"https://twitter.com/saad_benali_",
+          "className":"fa fa-twitter"
+        }
+      ],
+    "aboutme":"I am currently a  student at The sup-mti . I am a self taught Full Stack Web Developer, currently diving deeper into .net  . I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
+    "address":"maroc",
+    "website":"https://github.com/saadasl",
+    "education":[
+      {
+        "UniversityName":"Ecole Supérieur de Management, Télécommunication et d'Informatique",
+        "specialization":"genie informatique",
+       
+       
+      },
+      {
+        "UniversityName":"Ista",
+        "specialization":"Mecatronique",
+        
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Sqli-Oujda",
+        "specialization":"Devops",
+        
+        
+      },
+      {
+        "CompanyName":"Atlas Bottling",
+        "specialization":"Some specialization",
+       
+      }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":".Net"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/app1.png"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"user666"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"user-25"
+      }
+    ]
+  }
+  
+  export default resumeData
